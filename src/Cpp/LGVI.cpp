f80f110fc0ca67c4e2a98d9466e7d977438f4396
Yoonjae Hwang <yh583@cam.ac.uk>
2025-09-13 20:50:25 +0900

    Eigen installed succesfully

--- src/Cpp/LGVI.cpp
+++ src/Cpp/LGVI.cpp
@@ -1,4 +1,16 @@
 #include <iostream>
+#include<Eigen/Dense>
+
+using namespace std;
+using namespace Eigen;
+
+
 int main(){
+
+    Matrix <float, 2,2> A;
+    A << 1,2,
+         3,4;
+    cout << A << endl;
+    cout << "Hello WOrld!" << endl;
     return 0;
 }
